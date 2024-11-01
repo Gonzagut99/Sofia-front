fcedf2b7cabdf7f34089c50ad1f9a6606bbad261
joche3 <75068346@certus.edu.pe>
2024-11-01 17:31:16 -0500

    Comprobacion de rama

---
+++ app/routes/_index+/section+/index.tsx
@@ -0,0 +1,17 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return null;
+};
+
+import React from 'react'
+
+function index() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default index

ea6c8a7e27c482ba4fe33bb01c45d5daafe2b73c
joche3 <75068346@certus.edu.pe>
2024-11-01 19:47:37 -0500

    Agrego la pagina principal de Secciones

--- app/routes/_index+/section+/index.tsx
+++ app/routes/_index+/section+/index.tsx
@@ -1,4 +1,14 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { GraduationCap } from "lucide-react"
+import { Link } from "@remix-run/react";
+import TypingAnimation from "~/components/ui/typing-animation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   return null;
@@ -9,7 +19,77 @@ import React from 'react'
 function index() {
   return (
     <>
-      
+      <section className="flex flex-col
+      justify-center md:flex-row-reverse md:items-center gap-4 lg:gap-10">
+
+        <div className="flex flex-col gap-3 text-center w-full md:w-1/3">
+          <TypingAnimation
+            className="text-4xl leading-10 font-black bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text"
+            duration={80}
+            text="Mis secciones"
+          ></TypingAnimation>
+          <h2 className="text-xl"> Selecciona una sección para ver información de estudiantes, <span className="font-light">promedio general y más.</span> </h2>
+        </div>
+        <figure>
+          <img
+            src="/assets/TeacherStatistics.png"
+            alt="img-logo"
+            className="w-full md:w-3/4 mx-auto" />
+        </figure>
+      </section>
+
+      <section className="flex flex-col justify-center items-center gap-4">
+
+        <Link to="/none" className="w-full md:w-2/4">
+          <Card className="h-full w-full dark:bg-zinc-900 dark:hover:bg-zinc-800 bg-zinc-50 hover:bg-zinc-100 cursor-pointer">
+            <CardHeader>
+              <CardTitle className="flex flex-row gap-4">
+                <figure className="flex flex-col justify-start">
+                  <GraduationCap />
+                </figure>
+                <div className="flex flex-col justify-start gap-2 ">
+                  <h2 className="font-semibold">Sección: BTE-01</h2>
+                  <p className="font-light">Curso: Proyecto Integrador</p>
+                </div>
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </Link>
+
+        <Link to="/none" className="w-full md:w-2/4">
+          <Card className="h-full w-full dark:bg-zinc-900 dark:hover:bg-zinc-800 bg-zinc-50 hover:bg-zinc-100 cursor-pointer">
+            <CardHeader>
+              <CardTitle className="flex flex-row gap-4">
+                <figure className="flex flex-col justify-start">
+                  <GraduationCap />
+                </figure>
+                <div className="flex flex-col justify-start gap-2 ">
+                  <h2 className="font-semibold">Sección: KBJ-02</h2>
+                  <p className="font-light">Curso: Base de datos</p>
+                </div>
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </Link>
+
+        <Link to="none" className="w-full md:w-2/4">
+          <Card className="h-full w-full dark:bg-zinc-900 dark:hover:bg-zinc-800 bg-zinc-50 hover:bg-zinc-100 cursor-pointer">
+            <CardHeader>
+              <CardTitle className="flex flex-row gap-4">
+                <figure className="flex flex-col justify-start">
+                  <GraduationCap />
+                </figure>
+                <div className="flex flex-col justify-start gap-2 ">
+                  <h2 className="font-semibold">Sección: JSA-03</h2>
+                  <p className="font-light">Curso: Inteligencia Artificial</p>
+                </div>
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </Link>
+
+      </section>
+
     </>
   )
 }

17b80a3e44be99758964e71e779b953ed5153095
dennis2454Ps <76782124@certus.edu.pe>
2024-12-14 06:59:43 -0500

    registerchanges

--- app/types/user.ts
+++ app/types/user.ts
@@ -2,11 +2,11 @@ export type User = {
     id: string;
     email: string;
     password: string;
-    name: string;
-    lastname: string;
+    name?: string;
+    lastname?: string;
     username: string;
-    avatarUrl: string;
-    hashedRefreshToken: string;
+    avatarUrl?: string;
+    hashedRefreshToken?: string;
     createdAt: string;
     updatedAt: string;
 }
